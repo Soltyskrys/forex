5765cbcf667d004b7487024d059ae23e70867fc2
Soltyskrys <krystiansol@o2.pl>
2016-09-16 01:19:18 +0200

    Dodanie wykonywania skryptu z przykladowym parametrem
    Dodanie do przycisku ajaxa
    wykonanie skryptu php na serwerze który wykona skrypt pythona i zwróci
    wynik
    
    Na przyszłość - zdecydować się na POST/GET

--- public_html/pages/Application/fields/summary.php
+++ public_html/pages/Application/fields/summary.php
@@ -1,4 +1,6 @@
 <div id="summary" style="display:none">
     <h1>Summary</h1>
-    <button type="button" class="btn btn-success">Generate</button>
+    <button type="button" class="btn btn-success" onclick="appController.generateScript()">Generate</button>
+
+    <div id="code_div"></div>
 </div>
---
+++ resources/applicationScript/script.py
@@ -0,0 +1,6 @@
+#!/Python35/python.exe
+import sys
+#zwraca wartość ze standardowego wyjścia - tak jakbyć uruchamiał w konsoli
+print(sys.argv[1:])
+
+
